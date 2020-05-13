9c156affbb2be18daa19281bfc80825184e6444d
Fika Handani <fikaahandanii@gmail.com>
2020-05-13 18:15:30 +0700

    add access method

---
+++ src/akses/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "akses/library"
+
+func main() {
+	library.SayHello()
+}
---
+++ src/belajar-golang/method.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type student struct {
+	name  string
+	grade int
+}
+
+func (s student) sayHello() {
+	fmt.Println("Hallo", s.name)
+}
+
+func (s student) getNameAt(i int) string {
+	return strings.Split(s.name, " ")[i-1]
+}
+
+func main() {
+	var s1 = student{"Fika Handani", 20}
+	s1.sayHello()
+
+	var name = s1.getNameAt(2)
+	fmt.Println("My Name is : ", name)
+}
